47482f1c9ff49cb039d59b9b4ee50cb79ea1c205
AchmadAth <ahmadathoillah28@gmail.com>
2024-01-15 16:50:12 +0700

    gtag

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import CompanyOverview from "../components/CompanyOverview";
 import HeroSection from "../components/HeroSection";
 import Products from "../components/Products";
@@ -6,6 +7,18 @@ import Testimonials from "../components/Testimonials";
 export default function Home() {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-0YY71GGXKE"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0YY71GGXKE');
+          `}
+      </Script>
       <HeroSection />
       <CompanyOverview />
       <Products />
