eb38adedb43bfe63be5f9ddadd5ad7e5e657bbc1
AchmadAth <ahmadathoillah28@gmail.com>
2024-01-15 16:37:12 +0700

    ga4

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Inter } from "next/font/google";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Analytics gtag.js code */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-0YY71GGXKE"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0YY71GGXKE');
+          `}
+        </script>
+      </Head>
       <body className={inter.className}>
         <ChakraProvider>
           <Navbar />
